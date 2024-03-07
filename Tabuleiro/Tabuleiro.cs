a5c034f2a3bd9ec3b4202f8ba866f99c20349b14
LeandraCoutinho <leandracou123@gmail.com>
2024-03-07 08:57:55 -0300

    criando tabuleiro

---
+++ Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,20 @@
+namespace tabuleiro;
+
+public class Tabuleiro
+{
+    public int Linhas { get; set; }
+    public int Colunas { get; set; }
+    private Peca[,] pecas;
+
+    public Tabuleiro(int linhas, int colunas)
+    {
+        Linhas = linhas;
+        Colunas = colunas;
+        pecas = new Peca[linhas, colunas];
+    }
+
+    public Peca peca(int linha, int coluna)
+    {
+        return pecas[linha, coluna];
+    }
+}
